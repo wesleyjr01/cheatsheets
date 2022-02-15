5f089bf72bf96ac385afd87a68f9bfbe2ba4b8a7
Wesley Andre Bortolozo Junior <wesleyjr0101@gmail.com>
2022-02-15 09:04:07 -0400

    Update files_discovery.py

--- files_discovery.py
+++ files_discovery.py
@@ -1,17 +1,16 @@
-def get_files(filepath):
-    """Discover all files from filepath and return
-    a list of all filepaths.
+def get_files(folderpath)-> List[str]:
+    """Discover all files from folderpath and return a list of all filepaths.
     Parameters
     ----------
-    filepath : str
+    folderpath : str
         root dir of files
     Returns
     -------
     List
-        List of files paths
+        List of files in path
     """
     all_files = []
-    for root, dirs, files in os.walk(filepath):
+    for root, dirs, files in os.walk(folderpath):
         files = glob.glob(os.path.join(root, "*.json"))
         for f in files:
             all_files.append(os.path.abspath(f))
