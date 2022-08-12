faf900c289d91baab5bc29d49a888afc387a1c07
Wesley Bortolozo <wesleyjr0101@gmail.com>
2022-08-12 07:27:02 -0400

    added athena queries examples

---
+++ athena_queries.sql
@@ -0,0 +1,18 @@
+-- select id, count(*) from db.table group by 1 having count(*) > 1;
+
+
+-- WITH name_of_cte AS (
+-- SELECT *, extract(month from reportstartdate) as month, extract(year from reportstartdate) as year
+-- FROM "db"."table"
+-- ) 
+-- select * from name_of_cte
+
+
+-- CREATE TABLE IF NOT EXISTS default.test_wesley_db
+-- WITH (
+--     format = 'Parquet'
+--     ,parquet_compression='SNAPPY'
+--     ,external_location = 's3://bucketname/test_wesley_db/'
+-- ) AS
+-- SELECT * FROM db.table2
+-- ;
