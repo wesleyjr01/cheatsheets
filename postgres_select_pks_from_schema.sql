f2669ba1b2ce38523f7306a0825f7a67399cf7cb
Wesley Andre Bortolozo Junior <wesleyjr0101@gmail.com>
2023-05-05 14:31:00 -0300

    Update postgres_select_pks_from_schema.sql

--- postgres_select_pks_from_schema.sql
+++ postgres_select_pks_from_schema.sql
@@ -1,8 +1,10 @@
-SELECT conrelid::regclass AS table_name,
-       conname AS primary_key,
-       pg_get_constraintdef(oid)
-FROM pg_constraint 
-WHERE contype = 'p' 
-AND connamespace = 'rpt'::regnamespace
-ORDER BY conrelid::regclass
-;
+with cte_ as (
+	SELECT conrelid::regclass AS table_name,
+	       conname AS primary_key,
+	       pg_get_constraintdef(oid)
+	FROM pg_constraint 
+	WHERE contype = 'p' 
+	AND connamespace = 'product'::regnamespace
+	ORDER BY conrelid::regclass
+)
+select table_name, pg_get_constraintdef as PK from cte_;
