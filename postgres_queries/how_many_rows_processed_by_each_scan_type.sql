fd1eb25fe52713e39e84e4d3212acacf493b34fa
Wesley Andre Bortolozo Junior <wesleyjr0101@gmail.com>
2023-11-27 15:40:23 -0300

    Update how_many_rows_processed_by_each_scan_type.sql

--- postgres_queries/how_many_rows_processed_by_each_scan_type.sql
+++ postgres_queries/how_many_rows_processed_by_each_scan_type.sql
@@ -1,1 +1,5 @@
--
+SELECT relname, seq_tup_read, idx_tup_fetch,
+       cast(idx_tup_fetch AS numeric) / (idx_tup_fetch + seq_tup_read) 
+       AS idx_tup_pct 
+FROM pg_stat_user_tables 
+       WHERE (idx_tup_fetch + seq_tup_read) > 0 ORDER BY idx_tup_pct;
