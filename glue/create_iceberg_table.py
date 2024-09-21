75a6a95c870da5372ac6928647f30f848ca873be
Wesley Andre Bortolozo Junior <wesleyjr0101@gmail.com>
2024-09-21 13:03:39 -0400

    Update create_iceberg_table.py

--- glue/create_iceberg_table.py
+++ glue/create_iceberg_table.py
@@ -1,39 +1,60 @@
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from pyspark.sql.types import *
 
-%%configure -f
-{
-    "datalake-formats": "iceberg",
-    "--conf": "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO --conf spark.sql.catalog.glue_catalog.warehouse=s3://<bucket_name>/notebook/iceberg_tests/",
-    '--enable-glue-datacatalog': True
-}
 
-sc = SparkContext.getOrCreate()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+# Env Vars
+iceberg_db = "<iceberg_db>"
+iceberg_table = "<iceberg_table>"
+destination_s3_path = "s3://<bucketname>/iceberg"
+partition_by = "ingested_date"
+source_db = "<source_db>"
+source_table = "<source_table>"
+
+# Spark Session
+spark = (
+    SparkSession.builder.config(
+        "spark.sql.extensions",
+        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    )
+    .config("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    .config("spark.sql.catalog.glue_catalog.warehouse", destination_s3_path)
+    .config(
+        "spark.sql.catalog.glue_catalog.catalog-impl",
+        "org.apache.iceberg.aws.glue.GlueCatalog",
+    )
+    .config(
+        "spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO"
+    )
+    .getOrCreate()
+)
+# Add KMS Information to Write to S3 KMS Encrypted Buckets
+spark._jsc.hadoopConfiguration().set("fs.s3.enableServerSideEncryption", "true")
+
+# Glue Context
+glueContext = GlueContext(spark)
 job = Job(glueContext)
-dyf = glueContext.create_dynamic_frame.from_catalog(database='<source_db>', table_name='<source_table>')
-dyf.printSchema()
+# job.init(args["job_name"], args)
+
+
+# Import Source data
+dyf = glueContext.create_dynamic_frame.from_catalog(
+    database=source_db, table_name=source_table
+)
 df = dyf.toDF()
-df.show(3)
-iceberg_db = "iceberg_test"
-iceberg_table = "notebook_iceberg_ml_pred_table_sql"
 
-tmp_spark_table = "tmp_predictions"
-df.createOrReplaceTempView("tmp_predictions")
 
-# WORKED
+# Create Iceberg Table
+tmp_spark_table = "tmp_table"
+df.createOrReplaceTempView("tmp_table")
 query = f"""
 CREATE TABLE glue_catalog.{iceberg_db}.{iceberg_table}
 USING iceberg
-PARTITIONED BY (ingested_date)
+PARTITIONED BY ({partition_by})
 TBLPROPERTIES ("format-version"="2")
-AS SELECT is_core, document_id, ingested_date FROM {tmp_spark_table}
+AS SELECT * FROM {tmp_spark_table}
 """
 spark.sql(query)
 job.commit()
+
